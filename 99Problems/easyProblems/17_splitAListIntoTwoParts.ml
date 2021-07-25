668c6198723bbf83e723ff1e8813d183ea220932
Tiago Onofre <taraujonofre@gmail.com>
2021-07-24 23:14:49 -0300

    Split a list in two parts successful completed

--- 99Problems/easyProblems/17_splitAListIntoTwoParts.ml
+++ 99Problems/easyProblems/17_splitAListIntoTwoParts.ml
@@ -8,17 +8,29 @@ let split list len =
                   else aux (acc @ ([hd] :: List.tl acc) ) (count+1) tl in
   aux [[]] 1 list;;
 
+(** Second Try - Solved (thanks to the wine)*) 
+
+let split list len =
+  let add_to_tuple value (list1, list2) =
+    (list1, [value] @ list2) in
+  let rec aux acc count = function
+    | [] -> (acc, [])
+    | [element] -> if count <= len then (acc @ [element], []) else (acc, [element])
+    | hd :: tl -> if count <= len then aux (acc @ [hd]) (count+1) tl
+                  else add_to_tuple hd (aux acc (count+1) (tl)) in
+  aux [] 1 list;;
+
+
 (** OCaml.org *)
 
 let split list n =
     let rec aux i acc = function
       | [] -> List.rev acc, []
       | h :: t as l -> if i = 0 then List.rev acc, l
-                       else aux (i - 1) (h :: acc) t
+                       else aux (i - 1) (h :: acc)  t
     in
       aux n [] list;;
 
 (** OCaml.org type: 'a list -> int -> 'a list * 'a list = <fun> *)
 (** My type: 'a list -> int -> 'a list list = <fun> *)
 
-(** Need to review what exaclty as operator does *)
