88658e8ba7fe7b9246ebff5114ad1a14494f318a
Tiago Onofre <taraujonofre@gmail.com>
2021-08-30 18:01:30 -0300

    Database controller interface

---
+++ OpiumGuide/HelloWorldOpium/mini_crud_postgres/db_controller.mli
@@ -0,0 +1,15 @@
+open User_yojson
+
+type error =
+  | Database_error of string
+
+(* Migrations-related helper functions. *)
+val migrate : unit -> (unit, error) result Lwt.t
+val rollback : unit -> (unit, error) result Lwt.t
+
+(* Core functions *)
+val get_all : unit -> (UserJson.t list, error) result Lwt.t
+val add : UserJson.t -> (unit, error) result Lwt.t
+val remove : int -> (unit, error) result Lwt.t
+val clear : unit -> (unit, error) result Lwt.t
+
