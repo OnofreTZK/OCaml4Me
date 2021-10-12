5d043b96b6e77da4c80c59326f5caf0fcf5115c4
Tiago Onofre <taraujonofre@gmail.com>
2021-10-12 14:47:18 -0300

    Did it! Need to get back to docs to understand Lwt.bind, monads syntax and how to control my async lwt loop with lwt_main.run

--- testRequest/test_request.ml
+++ testRequest/test_request.ml
@@ -1,37 +1,29 @@
 type return = {
   message : string;
-  size : int
+  length : int
 }[@@deriving yojson]
 
 (* string Lwt.t *)
-let body = 
+let get_body =
   let open Lwt.Syntax 
   in
   let* req = Cohttp_lwt_unix.Client.get (Uri.of_string "http://192.168.1.217:4444/blockchain/mine") 
   in
-  req 
-   |> fun (resp, body) -> resp 
-   |> fun _ -> Cohttp_lwt.Body.to_string body 
-   |> fun lwt_str -> match Lwt.state lwt_str with 
-                      | Return str -> Lwt.return str 
-                      | Fail err -> raise err
-                      | Sleep -> Lwt.return "{message: parse failed, length: 0}"
+  req
+  |> fun (resp, body) -> resp |> fun _ -> body 
+  |> Cohttp_lwt.Body.to_string 
 
 (* Yojson.Safe.t *)
 let json = 
-    let open Lwt.Syntax 
-    in
-    let* str_body = body 
-    in
-    Yojson.Safe.from_string str_body
+    Yojson.Safe.from_string (Lwt_main.run get_body)
   
 let obj = 
     match [%of_yojson: return] json with
       | Ok ob -> ob
-      | Error err -> raise (Invalid_argument err)
+      | Error err -> Printf.printf "Error ja no json\n" |> fun () -> raise (Invalid_argument err)
 
 
-let () = Printf.printf "message: %s\n%!" obj.message |> fun () -> Printf.printf "size: %d\n%!" obj.size
+let () = Printf.printf "message: %s\n%!" obj.message |> fun () -> Printf.printf "size: %d\n%!" obj.length
 
 (*
 let body =
